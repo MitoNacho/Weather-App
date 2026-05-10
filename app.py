462ef3c3886bc4f0c60b72369f7fd25fa567d8e1
Nacho Naves <134378740+MitoNacho@users.noreply.github.com>
2026-05-10 15:56:42 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -47,9 +47,10 @@ if city:
 
     icon_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
 
-    st.markdown("""<style>.stApp {background: linear-gradient(to bottom, #87CEEB, #ffffff);}</style>""", unsafe_allow_html=True)
     
     st.image(icon_url, width=100)
+
+    st.markdown("""<style>.stApp {background: linear-gradient(to bottom, #87CEEB, #ffffff);}</style>""", unsafe_allow_html=True)
 
     st.subheader(desc.capitalize())
 
